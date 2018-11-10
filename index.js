ace9aa3e5998bd8cd44076944a03fad47871e731
Lee Arnold <lee.arnoldiii@gmail.com>
2018-11-10 08:38:28 -0600

    update to structure and functions in file

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-function renderMovies(movieData) {
+  function renderMovies(movieData) {
     let renameThis = ''
-    for (i=0; i < movieData.length; i++) {
-	renameThis += `
+    for (i = 0; i < movieData.length; i++) {
+      renameThis += `
 	<div class="movie"><img class="poster" src="${movieData[i].Poster}"/>
 	<h1>${movieData[i].Title}</h1>
     <span class="date">${movieData[i].Year}</span>
     <br>
-    <button type="button">Add to Favorites</button>
+    <button type="button" onclick="function saveToWatchlist(${movieData.imbdID}) {}">Add to Favorites</button>
 	</div>`
-	} return renameThis
-} console.log(renderMovies(movieData))
+    } return renameThis
+  } console.log(renderMovies(movieData))
 
-let content = document.querySelector('.movies-container')
+  function saveToWatchlist(imdbID) {
+    var movie = movieData.find(function (currentMovie) {
+      return currentMovie.imdbID == imdbID
+    })
+    var watchlistJSON = localStorage.getItem('watchlist')
+    var watchlist = JSON.parse(watchlistJSON)
+    if (watchlist === null) {
+      watchlist = []
+    }
+    watchlist.push(movie)
+    watchlistJSON = JSON.stringify(watchlist)
+    localStorage.setItem('watchlist', watchlistJSON)
 
-content.innerHTML = renderMovies(movieData)})
+  } 
+
+  let content = document.querySelector('.movies-container')
+
+  document.getElementById('search-form').addEventListener('submit', function(e) {
+    e.preventDefault()
+    content.innerHTML = renderMovies(movieData)
+  })
+
+})
